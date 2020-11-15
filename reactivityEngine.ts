21e6fa4ccc5bf95e8240b11857e7991785eadeca
Thom <tomica.trncic@gmail.com>
2020-11-15 20:12:51 +0100

    wip: update reactive and track and remove failing types

--- reactivityEngine.ts
+++ reactivityEngine.ts
@@ -2,32 +2,38 @@ let dependency: Set<any> = new Set();
 let dependencyMap: Map<string, any> = new Map();
 const targetMap: WeakMap<any, any> = new WeakMap();
 
-function reactive( target: Record<string, string | number> ) {
+function reactive( target ) {
 	const handler = {
-		get( target: Record<string, string | number>, key: string, receiver ) {
+		get( target, key: string, receiver ) {
 			console.log( 'Calling GET with ' + key );
-			return Reflect.get( target, key, receiver );
+			let _result = Reflect.get( target, key, receiver );
+			track( target, key );
+			return _result;
 		},
-		set( target: Record<string, string | number>, key: string, value, receiver ) {
+		set( target, key: string, value, receiver ) {
 			console.log( 'Calling SET with ' + key + 'and setting value with ' + value );
-			return Reflect.get( target, key, receiver );
+			let _oldValue = target[key];
+			let _result = Reflect.get( target, key, receiver );
+			if ( _result && _oldValue != value ) trigger( target, key );
+			return _result;
 		}
 	};
 	return new Proxy( target, handler );
 }
 
-function track( target: Record<string, string | number>, key: string ) {
+function track( target, key: string ) {
 	let _depsMap = targetMap.get( target );
-	let _dep = _depsMap.get( key );
-
 	if ( !_depsMap ) targetMap.set( target, ( _depsMap = new Map() ) );
+
+	let _dep = _depsMap.get( key );
 	if ( !_dep ) _depsMap.set( key, ( _dep = new Set() ) );
 	dependency.add( effect );
 }
 
-function trigger( target: Record<string, string | number>, key: string ) {
+function trigger( target, key: string ) {
 	const _depsMap = targetMap.get( target );
 	if ( !_depsMap ) return;
+
 	let _dependency = _depsMap.get( key );
 	if ( _dependency ) dependency.forEach( ( effect ) => effect() );
 }
@@ -35,6 +41,10 @@ function trigger( target: Record<string, string | number>, key: string ) {
 let testObj = reactive( { quantity: 4, price: 10 } );
 let total;
 
+console.log( 'testObj: ', testObj );
 let effect = () => { total = testObj.price * testObj.quantity; };
-track( testObj );
 effect();
+console.log( 'total: ', total );
+
+testObj.quantity = 15;
+console.log( 'total: ', total );
