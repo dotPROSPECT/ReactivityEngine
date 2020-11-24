921f7e6126da4f7a2400290257144377c084303e
Thom <tomica.trncic@gmail.com>
2020-11-24 23:04:20 +0100

    fix: replace Reflect.get from Proxy setter with Reflect.set

--- reactivityEngine.ts
+++ reactivityEngine.ts
@@ -1,53 +1,60 @@
+const DEV_ENV = true;
+
 const targetMap: WeakMap<any, any> = new WeakMap();
 let activeEffect: () => any = () => { };
 
 function _track( target, key: string ) {
 	if ( activeEffect ) {
-		console.log( '_track -> targetMap: ', targetMap );
+		DEV_ENV && console.log( '_track -> targetMap: ', targetMap );
 
 		let _dependencyMap = targetMap.get( target );
 		if ( !_dependencyMap ) targetMap.set( target, ( _dependencyMap = new Map() ) );
 
-		console.log( '_track._dependencyMap: ', _dependencyMap );
+		DEV_ENV && console.log( '_track._dependencyMap: ', _dependencyMap );
 
 		let _dependencySet = _dependencyMap.get( key );
 		if ( !_dependencySet ) _dependencyMap.set( key, ( _dependencySet = new Set() ) );
 		_dependencySet.add( activeEffect );
 
-		console.log( '_track._dependencySet: ', _dependencySet );
+		DEV_ENV && console.log( '_track._dependencySet: ', _dependencySet );
+		DEV_ENV && console.log( ' ' );
 	}
 }
 
 function _trigger( target, key: string ) {
-	const _depsMap = targetMap.get( target );
-	if ( !_depsMap ) return;
+	const _dependencyMap = targetMap.get( target );
+	if ( !_dependencyMap ) return;
 
-	console.log( '_trigger._depsMap: ', _depsMap );
+	DEV_ENV && console.log( '_trigger._dependencyMap: ', _dependencyMap );
 
-	let _dependencySet = _depsMap.get( key );
-	console.log( '_trigger._dependencySet: ', _dependencySet );
+	let _dependencySet = _dependencyMap.get( key );
 	if ( _dependencySet ) _dependencySet.forEach( ( effect ) => effect() );
 
+	DEV_ENV && console.log( '_trigger._dependencySet: ', _dependencySet );
+	DEV_ENV && console.log( ' ' );
 }
 
 export function reactive( target ) {
 	const handler = {
 		get( target, key: string, receiver ) {
-			console.log( 'Calling proxy [[ GET ]] with {{ ' + key + ' }} and getting value ' + target[key] );
+			DEV_ENV && console.log( 'Calling proxy [[ GET ]] with {{ ' + key + ' }} and getting value ' + target[key] );
+			DEV_ENV && console.log( ' ' );
 
 			let _result = Reflect.get( target, key, receiver );
 			_track( target, key );
 			return _result;
 		},
 		set( target, key: string, value, receiver ) {
-			console.log( 'Calling proxy [[ SET ]] with {{ ' + key + ' }} and setting value with ' + value );
+			DEV_ENV && console.log( 'Calling proxy [[ SET ]] with {{ ' + key + ' }} and setting value with ' + value );
+			DEV_ENV && console.log( ' ' );
 
 			let _oldValue = target[key];
-			let _result = Reflect.get( target, key, receiver );
+			let _result = Reflect.set( target, key, value, receiver );
 			if ( _result && _oldValue != value ) _trigger( target, key );
 			return _result;
 		}
 	};
+	DEV_ENV && console.log( ' ' );
 	return new Proxy( target, handler );
 }
 
@@ -60,13 +67,15 @@ export function effect( eff ) {
 export function ref( raw ) {
 	const wrapper = {
 		get value() {
-			console.log( 'Calling ref [[ GET ]] and getting value ' + raw );
+			DEV_ENV && console.log( 'Calling ref [[ GET ]] and getting value ' + raw );
+			DEV_ENV && console.log( ' ' );
 
 			_track( wrapper, 'value' );
 			return raw;
 		},
 		set value( newValue ) {
-			console.log( 'Calling ref [[ SET ]] and setting value with ' + newValue );
+			DEV_ENV && console.log( 'Calling ref [[ SET ]] and setting value with ' + newValue );
+			DEV_ENV && console.log( ' ' );
 
 			raw = newValue;
 			_trigger( wrapper, 'value' );
